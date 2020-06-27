6d64f4a550bcca48bb9f6e8b13e51bc0df2ec778
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-06-27 16:50:01 -0700

    NetworkTest.sh
    The Shell script that tests the network.

---
+++ Fedora/NetTest/NetworkTest.sh
@@ -0,0 +1,15 @@
+# Start of script
+
+# This script tests the network and troubleshoots issues
+
+# Script version: 1 (Saturday, June 27th 2020 at 4:50 pm)
+# File type: BASH shell script (*.sh)
+# Line count (including blank lines): 16
+
+# Pinging home
+ping 127.0.0.1
+
+# Other pings and steps
+# Currently unavailable
+
+# End of script
