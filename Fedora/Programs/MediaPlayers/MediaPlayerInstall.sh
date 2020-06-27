80dc4ccde17f75aed6d2640a89f10311bb4124e1
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-06-27 16:34:01 -0700

    MediaPlayerInstall.sh
    The Shell script that installs media players for me.

---
+++ Fedora/Programs/MediaPlayers/MediaPlayerInstall.sh
@@ -0,0 +1,15 @@
+# Start of script
+
+# This script is an installer script for media players
+
+# Script version: 1 (Saturday, June 27th 2020 at 4:34 pm)
+# File type: BASH shell script (*.sh)
+# Line count (including blank lines): 16
+
+# My default Media player
+dnf install @vlc-media-player # Installs VLC Media player
+
+# Some optional Media Players
+# I don't know of any at the moment
+
+# End of script
