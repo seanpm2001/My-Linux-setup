bb01e70d1f2579d4e276ff2234e305a4b6665939
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-06-27 16:39:40 -0700

    VMInstallV1.sh
    An archive of the first version of the Shell script that installs my office software.

---
+++ Fedora/Programs/Virtualization/OldVersions/VMInstallV1.sh
@@ -0,0 +1,15 @@
+# Start of script
+
+# This script is an installer script for virtualization software
+
+# Script version: 1 (Saturday, June 27th 2020 at 4:39 pm)
+# File type: BASH shell script (*.sh)
+# Line count (including blank lines): 16
+
+# My default Virtualization software
+dnf install @lvirtualbox # Installs VirtualBox
+
+# Some optional Virtualization Software
+dnf install @qemu # Installs Qemu
+
+# End of script
