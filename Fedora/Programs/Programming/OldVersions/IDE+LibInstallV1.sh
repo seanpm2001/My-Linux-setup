adf22de69dd43464ced7bd6922029db0d9846876
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-06-27 16:44:45 -0700

    IDE+LibInstallV1.sh
    An archive of the first version of the Shell script that installs my default IDEs and programming language libraries.

---
+++ Fedora/Programs/Programming/OldVersions/IDE+LibInstallV1.sh
@@ -0,0 +1,21 @@
+# Start of script
+
+# This script is an installer script for IDEs and libraries
+
+# Script version: 1 (Saturday, June 27th 2020 at 4:44 pm)
+# File type: BASH shell script (*.sh)
+# Line count (including blank lines): 22
+
+# My default IDEs
+dnf install @python-3 # Installs Python 3
+# dnf install @notepad++ # I need to find an alternate for notepad++
+dnf install @scratch-2 # Installs Scratch 2
+dnf install @scratch-3 # Installs Scratch 3 when the Linux port is ready
+
+# Some optional IDEs and libraries
+dnf install @java # Installs Java
+dnf install @ruby # Installs Ruby
+dnf install @php-7 # Installs PHP 7
+# No other IDEs at the moment
+
+# End of script
