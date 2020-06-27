194f5839671f4febd1aa422456d91e0ff3fa8a57
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-06-27 16:31:12 -0700

    WebBrowserInstall.sh
    The script that installs web browsers.

---
+++ Fedora/Programs/WebBrowsers/WebBrowserInstall.sh
@@ -0,0 +1,17 @@
+# Start of script
+
+# This script installs browsers for my Linux setup
+
+# File version: 1 (Saturday, June 27th 2020 at 4:31 pm)
+# Line count (including blank lines): 18
+# File type: BASH Shell script (*.sh)
+
+# Default browser
+dnf install @firefox # Installs the Firefox web browser
+
+# Alternate browsers
+dnf install @konquerer # Installs the Konquerer web browser
+dnf install @icecat # Installs the GNU IceCat web browser
+dnf install @seamonkey # Installs the SeaMonkey Internet Suite
+
+# End of script
