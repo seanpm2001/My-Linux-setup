87936d3322fc2bd3e1fc94bad1749962169210aa
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-06-27 16:47:17 -0700

    DesignInstall.sh
    The Shell script that installs my graphic design software.

---
+++ Fedora/Programs/GraphicDesign/DesignInstall.sh
@@ -0,0 +1,16 @@
+# Start of script
+
+# This script is an installer script for Graphic Design software
+
+# Script version: 1 (Saturday, June 27th 2020 at 4:47 pm)
+# File type: BASH shell script (*.sh)
+# Line count (including blank lines): 17
+
+# My default graphic design software
+dnf install @gimp # Installs GIMP
+dnf install @inkscape # Installs Inkscape (I am looking forward to this when I install Linux, it will be cool to have software that can make and edit SVG files)
+
+# Some optional graphic design software
+# No other Graphic Design software packages at the moment
+
+# End of script
