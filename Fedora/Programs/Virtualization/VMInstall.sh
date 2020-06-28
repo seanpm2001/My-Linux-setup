7e648e9ea1478ae64c152ec6e5763840a22f5662
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-06-27 17:10:07 -0700

    VMInstall.sh version 2
    The 2nd version of the virtualization software installer script. This version fixes a critical spelling error for virtualbox installation.

--- Fedora/Programs/Virtualization/VMInstall.sh
+++ Fedora/Programs/Virtualization/VMInstall.sh
@@ -2,12 +2,12 @@
 
 # This script is an installer script for virtualization software
 
-# Script version: 1 (Saturday, June 27th 2020 at 4:39 pm)
+# Script version: 2 (Saturday, June 27th 2020 at 5:10 pm)
 # File type: BASH shell script (*.sh)
 # Line count (including blank lines): 16
 
 # My default Virtualization software
-dnf install @lvirtualbox # Installs VirtualBox
+dnf install @virtualbox # Installs VirtualBox
 
 # Some optional Virtualization Software
 dnf install @qemu # Installs Qemu
