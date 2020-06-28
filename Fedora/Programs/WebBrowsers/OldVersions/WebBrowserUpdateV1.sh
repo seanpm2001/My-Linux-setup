e4d21afb277e4137c96d3cc141808370e9470064
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-06-27 17:08:55 -0700

    WebBrowserUpdateV1.sh
    An archive of the first version of the web browser BASH update script.

---
+++ Fedora/Programs/WebBrowsers/OldVersions/WebBrowserUpdateV1.sh
@@ -0,0 +1,17 @@
+# Start of script
+
+# This script Updates browsers for my Linux setup
+
+# File version: 1 (Saturday, June 27th 2020 at 5:08 pm)
+# Line count (including blank lines): 18
+# File type: BASH Shell script (*.sh)
+
+# Default browser
+dnf update @firefox # Updates the Firefox web browser
+
+# Alternate browsers
+dnf update @konquerer # Updates the Konquerer web browser
+dnf update @icecat # Updates the GNU IceCat web browser
+dnf update @seamonkey # Updates the SeaMonkey Internet Suite
+
+# End of script
