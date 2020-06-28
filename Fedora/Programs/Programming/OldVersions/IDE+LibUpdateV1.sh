aa727ef7dd6987c342101e38c1164d13af67122b
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-06-27 17:04:35 -0700

    IDE+LibUpdateV1.sh
    An archive of the first version of the IDE and Library update BASH Shell script file.

---
+++ Fedora/Programs/Programming/OldVersions/IDE+LibUpdateV1.sh
@@ -0,0 +1,21 @@
+# Start of script
+
+# This script is an updater script for IDEs and libraries
+
+# Script version: 1 (Saturday, June 27th 2020 at 5:04 pm)
+# File type: BASH shell script (*.sh)
+# Line count (including blank lines): 22
+
+# My default IDEs
+dnf updater @python-3 # Updates Python 3
+# dnf update @notepad++ # I need to find an alternate for notepad++
+dnf update @scratch-2 # Updates Scratch 2
+dnf update @scratch-3 # Updates Scratch 3 when the Linux port is ready
+
+# Some optional IDEs and libraries
+dnf update @java # Updates Java
+dnf update @ruby # Updates Ruby
+dnf update @php-7 # Updates PHP 7
+# No other IDEs at the moment
+
+# End of script
