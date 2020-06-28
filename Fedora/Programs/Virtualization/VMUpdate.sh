4a7d1b12cb6bf43e0bf5715c966c47d33b0d4cb9
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-06-27 17:06:01 -0700

    VMUpdate.sh
    The Shell script that updates my Virtualization software.

---
+++ Fedora/Programs/Virtualization/VMUpdate.sh
@@ -0,0 +1,15 @@
+# Start of script
+
+# This script is an Updater script for virtualization software
+
+# Script version: 1 (Saturday, June 27th 2020 at 5:06 pm)
+# File type: BASH shell script (*.sh)
+# Line count (including blank lines): 16
+
+# My default Virtualization software
+dnf update @virtualbox # Updates VirtualBox
+
+# Some optional Virtualization Software
+dnf update @qemu # Updates Qemu
+
+# End of script
