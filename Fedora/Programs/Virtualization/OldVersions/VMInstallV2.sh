ff77830252376b34a71342aeba1dc66ec107a3a1
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-06-27 17:10:41 -0700

    VMInstallV2.sh
    An archive of the 2nd version of the virtualization software installation BASH script.

---
+++ Fedora/Programs/Virtualization/OldVersions/VMInstallV2.sh
@@ -0,0 +1,15 @@
+# Start of script
+
+# This script is an installer script for virtualization software
+
+# Script version: 2 (Saturday, June 27th 2020 at 5:10 pm)
+# File type: BASH shell script (*.sh)
+# Line count (including blank lines): 16
+
+# My default Virtualization software
+dnf install @virtualbox # Installs VirtualBox
+
+# Some optional Virtualization Software
+dnf install @qemu # Installs Qemu
+
+# End of script
