0ea5b147ef1de74118c5422f84eedf23e1817350
Sean P. Myrick V19.0.3.1 <65933340+seanpm2001@users.noreply.github.com>
2020-07-12 16:40:04 -0700

    Update to add support for battery percentage
    A small update that turns on battery percentage.

--- DE/GNOME/GNOME_Setup.sh
+++ DE/GNOME/GNOME_Setup.sh
@@ -1,4 +1,6 @@
 
+# Start of script
+
 # GNOME desktop environment setup
 
 # Clock
@@ -13,4 +15,26 @@ sudo gsettings set org.gnome.desktop.interface clock-show-seconds false
 
 gsettings get org.gnome.desktop.interface clock-show-seconds
 
+# Battery percentage
+
+gsettings set org.gnome.desktop.interface show-battery-percentage true
+
+# For if you want to turn it off (sudo is just for confirmation, just enter the password wrong thrice, and it will skip this)
+
+sudo gsettings set org.gnome.desktop.interface show-battery-percentage false
+
+# For checking
+
+gsettings get org.gnome.desktop.interface show-battery-percentage
+
 # That is all I have at the moment. I will experiment more later.
+
+# File info
+
+# File type: Bash Shell script (*.sh)
+
+# File version: 2 (Sunday, July 12th 2020 at 4:40 pm)
+
+# Line count (including blank lines and compiler line): 41
+
+# End of script
