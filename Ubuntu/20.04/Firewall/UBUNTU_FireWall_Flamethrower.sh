59c229ebb981fd6a5563b748718cb484e3fe69ea
Sean P. Myrick V19.0.3.1 <65933340+seanpm2001@users.noreply.github.com>
2020-07-12 17:05:01 -0700

    UBUNTU_FireWall_Flamethrower.sh
    A Ubuntu shell script file that enables/disables the system Firewall.

---
+++ Ubuntu/20.04/Firewall/UBUNTU_FireWall_Flamethrower.sh
@@ -0,0 +1,22 @@
+
+# Start of script
+
+# This script enables and disables the Firewall on Ubuntu
+
+# This will turn on the firewall. If you want to disable the firewall, enter your root password wrong thrice.
+
+sudo ufw enable # Sudo is my current way of a y/n on commands, it is inefficient, and I hope to find an alternate soon
+
+# This will turn off the firewall. If you want to enable the firewall, enter your root password wrong thrice and run this script again.
+
+sudo ufw disable # Sudo is my current way of a y/n on commands, it is inefficient, and I hope to find an alternate soon
+
+# File info
+
+# File type: Bash shell script (*.sh)
+
+# File version: 1 (Sunday, July 12th 2020 at 5:05 pm)
+
+# Line count (including blank lines and compiler line): 23
+
+# End of script
