50aca77e47747f983af2c9eb2d5532340815bcf5
Sean P. Myrick V19.0.3.1 <65933340+seanpm2001@users.noreply.github.com>
2020-07-15 09:56:38 -0700

    AudioRepairV1.sh
    An archive of the first version of the audio repair Shell script.

---
+++ Ubuntu/20.04/AudioRepair/OldVersions/AudioRepairV1.sh
@@ -0,0 +1,20 @@
+
+# Start of script
+
+# Method 1 - restore drivers
+
+alsactl restore # This method helped me restore my headphones on July 15th 2020
+
+sudo alsactl restore # If you require administrator privileges for this:
+
+# Method 2 - No other known software methods
+
+# Try checking your hardware, make sure it is fully plugged in
+
+# File info
+
+# File version: 1 (Wednesday, July 15th 2020 at 9:56 am)
+# File type: Bash Shell script (*.sh)
+# Line count (including blank lines and compiler line: 21
+
+# End of script
