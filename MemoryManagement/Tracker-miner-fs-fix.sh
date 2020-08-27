1b89896a8ef56badea5819afc3866c53c839b569
Sean P. Myrick V19.0.4.2 <65933340+seanpm2001@users.noreply.github.com>
2020-08-27 10:58:35 -0700

    Tracker-miner-fs-fix.sh
    A fix to the tracker-store freeze.

---
+++ MemoryManagement/Tracker-miner-fs-fix.sh
@@ -0,0 +1,4 @@
+# Only run this script when Tracker-miner-fs is running for more than 20 minutes and using up at least 6% CPU
+# Resetting the tracker can significantly save your laptop battery life by over 9 hours. It may cause data loss though
+tracker reset --hard
+# Input "y"
