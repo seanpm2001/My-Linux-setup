aae4384703ddefaa9b41acc9a0de2312063a02d6
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2020-10-25 17:16:02 -0700

    Neo~Neofetch.sh
    Creates the Neo alias. I made this to test

---
+++ CLI/BASH/sh/Alias/Neo~Neofetch.sh
@@ -0,0 +1,11 @@
+# This file makes the "neo" alias, which will redirect to neofetch
+# Start of script
+
+alias neo="neofetch"
+
+# End of script
+# This script is a test of Linux command-line aliases. I noted that there aren't many very important programs you would want to open via Konsole/whatever Terminal you use that have this name, so I applied it to neofetch.
+# File name: Neo~Neofetch.sh
+# File type: Bourne Again Shell Script (*.sh)
+# File version: 1 (Sunday, October 25th 2020 at 5:16 pm)
+# Line count (including blank lines and compiler line): 13
