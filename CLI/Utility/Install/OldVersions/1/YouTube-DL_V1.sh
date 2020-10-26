2043942f8e5bbafdb495a5ae967eb8a4f71299b3
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2020-10-25 17:30:44 -0700

    YouTube-DL_V1.sh
    An archive of the first version of the YouTube-DL installation script.

---
+++ CLI/Utility/Install/OldVersions/1/YouTube-DL_V1.sh
@@ -0,0 +1,10 @@
+# YouTube-dl
+# Unfortunately, YouTube-DL was taken down from GitHub due to an unfair DMCA complaint made by the RIAA. They seem to be going back to their useless, childish Napster methods.
+# Note to the RIAA: Taking down 1 YouTube video downloader is not going to do anything to help your cause. You just cut the hydras head again, this hydra has over a million heads due to how many times it has been cut.
+# I really enjoyed YouTube-DL, it was my 9th video downloading tool, and it was the best working one out of all of them so far. I am hoping the people being YouTube-DL can stand up to the RIAA and make an objection
+# Start of script
+# Ubuntu
+sudo apt-install youtube-dl # Method 1
+sudo apt-get youtube-dl # Method 2
+sudo install youtube-dl # Method 3
+# End of script
