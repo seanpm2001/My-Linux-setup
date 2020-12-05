dfe7b870957e7e6e868b842ed315e6d99b93acd3
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2020-12-04 16:40:31 -0800

    XTERM_INSTALL_V1.sh
    An archive of the first version of the XTERM Installation script.

---
+++ X_Dot_ORG/Installs/OldVersions/XTERM_INSTALL_V1.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# xorg goodies - Install xterm
+
+sudo apt install xterm # Installs the X.Org terminal, as it normally doesn't come installed
+xterm
+# Old program, please use the all-in-one x.org program for better result
