9c982d05621d2373ba4d03f527b71d6c938308b3
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2020-12-31 21:37:13 -0800

    Added support for day of week
    New update to tweak GNOME.

--- DE/GNOME/GNOME_Setup.sh
+++ DE/GNOME/GNOME_Setup.sh
@@ -15,6 +15,18 @@ sudo gsettings set org.gnome.desktop.interface clock-show-seconds false
 
 gsettings get org.gnome.desktop.interface clock-show-seconds
 
+# For the day of week
+
+gsettings set org.gnome.desktop.interface clock-show-weekday true
+
+# For if you want to turn it off (sudo is just for confirmation, just enter the password wrong thrice, and it will skip this)
+
+gsettings set org.gnome.desktop.interface clock-show-weekday false
+
+# For some stupid reason, GNOME doesn't support display of the year in the titlebar. I find this frustrating, especially on December 31st 2020.
+
+\..
+
 # Battery percentage
 
 gsettings set org.gnome.desktop.interface show-battery-percentage true
@@ -33,8 +45,8 @@ gsettings get org.gnome.desktop.interface show-battery-percentage
 
 # File type: Bash Shell script (*.sh)
 
-# File version: 2 (Sunday, July 12th 2020 at 4:40 pm)
+# File version: 3 (Thursday, December 31st 2020 at 9:37 pm)
 
-# Line count (including blank lines and compiler line): 41
+# Line count (including blank lines and compiler line): 53
 
 # End of script
