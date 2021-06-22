45e03008a1dcccd271c1499d2fda1c8c9f256bce
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-06-22 13:24:58 -0700

    1752CalendarReform.sh
    The reformation of the Calendar (1752)

---
+++ Extras/1752Reform/1752CalendarReform.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# In 1752, the British Empire and its territories switched from the Julian calendar to the Gregorian calendar. To adjust the days for this change, September 3rd to September 13th 1752 were skipped for this year
+# This was included into the Calendar command and was pointed out by its developer, although some may consider it a bug, as this calendar change did not happen in all nations (some countries adopted the Gregorian Calendar before or after 1752)
+cal 1752
+cal 9 1752
