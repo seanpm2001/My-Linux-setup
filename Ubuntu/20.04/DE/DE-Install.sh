001498666a19ea7866940fef509f62610c8db6b9
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-11-26 15:33:02 -0800

    DE-Install.sh (V1)
    Version 1 of the desktop environment installation script for Ubuntu 2004.

---
+++ Ubuntu/20.04/DE/DE-Install.sh
@@ -0,0 +1,16 @@
+# Start of script
+
+# This script is an installer script for desktop environments
+
+# Script version: 1 (Friday, 2021 November 26th at 3:33 pm
+# File type: BASH shell script (*.sh)
+# Line count (including blank lines): 17
+
+# My default Desktop Environment
+sudo apt-install @kde-desktop # Installs the KDE desktop environment
+
+# Some optional desktop environments
+sudo apt-install @xfce # Installs the XFCE Desktop environment
+sudo apt-install @gnome # Installs the GNOME desktop environment
+
+# End of script
