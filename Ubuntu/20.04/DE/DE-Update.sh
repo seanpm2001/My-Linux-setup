d630d9ff7a2a1f47b437ea5936c76a5fcb1a68ef
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-11-26 15:30:01 -0800

    DE-Update.sh (V1)
    Version 1 of the script for updating the desktop environment in Ubuntu.

---
+++ Ubuntu/20.04/DE/DE-Update.sh
@@ -0,0 +1,16 @@
+# Start of script
+
+# This script is an updater script for desktop environments
+
+# Script version: 1 (Friday, 2021 November 26th at 3:30 pm)
+# File type: BASH shell script (*.sh)
+# Line count (including blank lines): 17
+
+# My default Desktop Environment
+sudo apt-update @kde-desktop # Updates the KDE desktop environment
+
+# Some optional desktop environments
+sudo apt-update @xfce # Updates the XFCE Desktop environment
+sudo apt-update @gnome # Updates the GNOME desktop environment
+
+# End of script
