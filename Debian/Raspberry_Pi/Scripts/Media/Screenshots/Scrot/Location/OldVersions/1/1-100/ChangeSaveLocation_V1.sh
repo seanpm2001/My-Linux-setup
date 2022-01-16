51b988e0a825b2ee1e6e0529b4a03ebfa89cf26e
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-01-15 16:34:37 -0800

    ChangeSaveLocation_V1.sh
    An archive of the first version of the script to change the screenshot save location.

---
+++ Debian/Raspberry_Pi/Scripts/Media/Screenshots/Scrot/Location/OldVersions/1/1-100/ChangeSaveLocation_V1.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# Change the location of a screenshot
+# 4 different variations, currently don't know how to switch between them
+# Path V1
+scrot /home/pi/Pictures/New_Screenshots/Unsorted/
+# Path V2
+scrot /home/pi/Pictures/New_Screenshots/
+# Path V3
+scrot /home/pi/Pictures/
+# Path V4
+scrot /home/pi/Pictures/Screenshots/
+# File info
+# File type: Shell source file (*.sh)
+# File version: 1 (2022, Saturday, January 15th at 4:33 pm)
+# Line count (including blank lines and compiler line): 17
+# End of script
