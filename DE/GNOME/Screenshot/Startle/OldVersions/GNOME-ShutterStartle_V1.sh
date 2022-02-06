9b76e52a797daf8080eedb52eda1906d476b5167
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-02-05 23:35:32 -0800

    GNOME-ShutterStartle_V1.sh
    An archive of the first version of the GNOME ShutterStartle script.

---
+++ DE/GNOME/Screenshot/Startle/OldVersions/GNOME-ShutterStartle_V1.sh
@@ -0,0 +1,6 @@
+#!bin/sh
+# GNOME ShutterStartle
+# Startle yourself with a random screenshot. By the time it activates, you will likely have forgotten about it, and it (is supposed to, or will) startle you
+gnome-screenshot -d 987 # Activates after 16 minutes and 27 seconds, long and random
+# File version: 1 (2022, Saturday, February 5th at 11:35 pm)
+exit
